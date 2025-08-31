e432a2d2bd362d5d4046313b34d3e1c977a6cbd8
aman-sanin <amansaninkmct@gmail.com>
2025-08-31 17:41:21 +0530

    Init Commit

---
+++ android/app/src/main/kotlin/com/example/rune/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rune
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+void main() {
+  runApp(const RuneApp());
+}
+
+class RuneApp extends StatefulWidget {
+  const RuneApp({super.key});
+
+  @override
+  State<RuneApp> createState() => _RuneAppState();
+}
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: _focusedDay,
+      firstDay: DateTime.utc(2020, 1, 1),
+      lastDay: DateTime.utc(2030, 12, 31),
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay; // update the focused day
+        });
+      },
+      calendarStyle: const CalendarStyle(
+        todayDecoration: BoxDecoration(
+          color: Colors.deepPurple,
+          shape: BoxShape.circle,
+        ),
+        selectedDecoration: BoxDecoration(
+          color: Colors.orange,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
+
+class _RuneAppState extends State<RuneApp> {
+  int _currentIndex = 0; // Track the current index of active tab
+
+  final _screens = const [CalendarScreen(), Center(child: Text('Tasks'))];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Rune'), centerTitle: true),
+        body: _screens[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: 'Calendar',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.check_box),
+              label: 'Tasks',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
